6da8767f13ff661b043f9262ed2035c3f94999ba
Nasnast <nasnast@ya.ru>
2024-05-02 23:53:41 +0300

    new task scripts

---
+++ scripts/user_interface.py
@@ -0,0 +1,36 @@
+def interface():
+    with open("notebook.json", "a", encoding="utf-8"):
+        pass
+
+    var = 0
+    while var != "4":
+        print(
+            "Меню:\n"
+            "1. Добавить заметку\n"
+            "2. Вывести на экран все заметки\n"
+            "3. Редактировать заметку"
+            "4. Поиск заметок\n"
+            "5. Удалить заметку"
+            "6. Выход"
+        )
+        print()
+        var = input("выберите вариант действия: ")
+        while var not in ("1", "2", "3", "4", "5", "6"):
+            print("некорректный ввод!")
+            var = input("выберите вариант действия: ")
+        print()
+
+        match var:
+            case "1":
+                add_notes()
+            case "2":
+                print_notes()
+            case "3":
+                edit_notes()
+            case "4":
+                search_notes()
+            case "5":
+                delete_notes()
+            case "6":
+                print("До свидания")
+        print()
