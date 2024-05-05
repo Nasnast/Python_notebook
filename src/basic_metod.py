1ff8cb411b7e9de99667d14aa698a926d4502cbc
Nasnast <nasnast@ya.ru>
2024-05-05 03:10:09 +0300

    файл .json

--- src/basic_metod.py
+++ src/basic_metod.py
@@ -32,7 +32,7 @@ def new_id():
 def add_notes():  # 1 создание заметки
     title = input("Введите заголовок заметки: ")
     text = input("Введите текст заметки: ")
-    date = datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     # id_len = len(notes) + 1
     note = {"id": new_id() + 1, "title": title, "text": text, "date": date}
     notes.append(note)
@@ -41,11 +41,14 @@ def add_notes():  # 1 создание заметки
 
 
 def print_notes():  # 2 вывод списка заметок
-    print("Список заметок")
-    for note in notes:
-        print(
-            f"ID: {note['id']}, Заголовок: {note['title']}, Текст: {note['text']}, Дата: {note['date']}"
-        )
+    print("Список заметок:")
+    if len(notes) == 0:
+        print("здесь пока ничего нет :-(")
+    else:
+        for note in notes:
+            print(
+                f"ID: {note['id']}, Заголовок: {note['title']}, Текст: {note['text']}, Дата: {note['date']}"
+            )
 
 
 def edit_notes():  # 3 редактирование заметки
@@ -57,22 +60,21 @@ def edit_notes():  # 3 редактирование заметки
             )
             note["title"] = input("Введите новый заголовок заметки: ")
             note["text"] = input("Введите новый текст заметки: ")
-            note["date"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            note["date"] = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
             save_notes()
             return print("Заметка отредактирована.")
     return print(f"Заметка с ID - {id} не найдена.")
 
 
 def search_notes():  # 4 поиск по дате
-    filter_date = input("Введите дату для фильтрации (дд-мм-гггг): ")
+    filter_date = input("Введите дату для фильтрации (дд/мм/гггг): ")
     for note in notes:
         if filter_date in note["date"]:
             print(
                 f"ID: {note['id']}, Заголовок: {note['title']}, Текст: {note['text']}, Дата: {note['date']}"
             )
-        # if not any(filter_date in note["date"] for note in notes):
-        else:
-            print("Заметок с такой датой не найдено.")
+    if not any(filter_date in note["date"] for note in notes):
+        print("Заметок с такой датой не найдено.")
 
 
 def delete_notes():  # 5 удаление
@@ -81,7 +83,5 @@ def delete_notes():  # 5 удаление
         if note["id"] == id:
             notes.remove(note)
             save_notes()
-            print("Заметка удалена.")
-            return len(notes) + 1
-            # return id_len
+            return print(f"Заметка с ID - {id} удалена.")
     return print(f"Заметка с ID - {id} не найдена.")
